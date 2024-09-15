4e2a20bb468315701e34d65f3729c817ed0c8386
z <z@z.z>
2024-09-15 04:11:59 -0700

    Updated

--- src/client/java/org/_123/fjskdfj/client/FjskdfjClient.java
+++ src/client/java/org/_123/fjskdfj/client/FjskdfjClient.java
@@ -1,10 +1,15 @@
 package org._123.fjskdfj.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 
 public class FjskdfjClient implements ClientModInitializer {
+    private static final Logger LOGGER = LogManager.getLogger("Fjskdfj");
 
     @Override
     public void onInitializeClient() {
+        LOGGER.info("mod running lalala");
     }
 }
