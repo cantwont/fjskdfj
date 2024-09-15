147cefc5b2bf8fdd4922161f2f6b5bfc656983ef
z <z@z.z>
2024-09-15 03:57:12 -0700

    test

---
+++ src/client/java/org/_123/fjskdfj/client/FjskdfjClient.java
@@ -0,0 +1,10 @@
+package org._123.fjskdfj.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FjskdfjClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/_123/fjskdfj/Fjskdfj.java
@@ -0,0 +1,10 @@
+package org._123.fjskdfj;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Fjskdfj implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
